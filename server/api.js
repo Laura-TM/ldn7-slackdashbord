ab057c267bb4e6a37e5c20a78ff7107e2b964a7c
askinekinci <askinekincihf@gmail.com>
2021-07-15 10:49:00 +0100

    fetched Slack API data

--- server/api.js
+++ server/api.js
@@ -1,9 +1,43 @@
 import { Router } from "express";
+import process from "process";
 
 const router = new Router();
+const axios = require("axios");
 
 router.get("/", (_, res) => {
-	res.json({ message: "Hello, world!" });
+	res.json({
+		message: "Hello, world!",
+	});
+});
+
+router.get("/channelList", async (request, response) => {
+	const slackToken = process.env.SLACK_API_TOKEN;
+	const url = "https://ldn7-test-workspace.slack.com/api/conversations.list";
+	console.log(slackToken);
+	console.log(process.env);
+	const res = await axios.get(url, {
+		headers: { Authorization: `Bearer ${slackToken}` },
+	});
+	response.json(res.data);
+});
+
+router.get("/userList", async (request, response) => {
+	const slackToken = process.env.SLACK_API_TOKEN;
+	const url = "https://ldn7-test-workspace.slack.com/api/users.list";
+	const res = await axios.get(url, {
+		headers: { Authorization: `Bearer ${slackToken}` },
+	});
+	response.json(res.data);
+});
+
+router.get("/channelHistory", async (request, response) => {
+	const slackToken = process.env.SLACK_API_TOKEN;
+	// const { channel } = request.params;
+	const url = `https://ldn7-test-workspace.slack.com/api/conversations.history?channel=C027M110K9T`;
+	const res = await axios.get(url, {
+		headers: { Authorization: `Bearer ${slackToken}` },
+	});
+	response.json(res.data);
 });
 
 export default router;
