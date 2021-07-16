93c5003d0b714e93bcf1e066ed589d0937fdbef5
Diba <72705126+DibaKarimi@users.noreply.github.com>
2021-07-16 20:57:11 +0100

    extact all statistics

--- server/api.js
+++ server/api.js
@@ -3,42 +3,57 @@ const router = new Router();
 
 router.get("/user/:userId", async (req, res) => {
 	const userId = req.params.userId;
-	const duration =
-		req.query.duration == "month"
-			? new Date() / 1000 - 60 * 60 * 30 * 24
-			: new Date() / 1000 - 60 * 60 * 7 * 24;
-	console.log(duration);
+	let result = {
+		userName: "",
+		statistics: [],
+	};
+	const now = new Date();
+	let time = req.query.time == "month" ? now.getMonth() : now.getDay();
+	let latest = now / 1000;
+	let oldest = latest - 60 * 60 * 24 * time;
+	time = req.query.time == "month" ? 30 : 7;
+	console.log("duration=", time, " latest=", latest, " oldest=", oldest);
+	const userInfo = await getUserInfo(userId);
+	const userName = userInfo.ok
+		? userInfo.user.real_name
+		: res.status(400).json("The user not found");
+	result.userName = userName;
+	console.log(userName);
 	const findReaction = (reactions) => {
 		return reactions.filter((reaction) => reaction.users.includes(userId));
 	};
-	const data = await getChannelHistory("C028DN05PUG", duration);
-	if (data.ok) {
-		const messageCount = data.messages.filter(
+	let data = await getChannelHistory("C028DN05PUG");
+	console.log(data);
+	if (data.ok && data.messages) {
+		const timestampOfJoin = data.messages.find(
 			(message) =>
-				(message.user == userId && message.type == "message") ||
-				(message.reply_users && message.reply_users.includes(userId))
-		).length;
-
-		const reactionCount = data.messages.filter(
-			(message) => message.reactions && findReaction(message.reactions)
-		).length;
-
-		const userInfo = await getUserInfo(userId);
-		if (userInfo.ok) {
-			const result = {
-				userName: userInfo.user.real_name,
+				message.type == "message" &&
+				message.subtype &&
+				message.subtype == "channel_join" &&
+				message.user == userId
+		);
+		console.log(timestampOfJoin.ts);
+		while (latest > timestampOfJoin.ts) {
+			data = await getChannelHistory("C028DN05PUG", oldest, latest);
+			const messageCount = data.messages.filter(
+				(message) =>
+					(message.user == userId && message.type == "message") ||
+					(message.reply_users && message.reply_users.includes(userId))
+			).length;
+			console.log(messageCount);
+			const reactionCount = data.messages.filter(
+				(message) => message.reactions && findReaction(message.reactions)
+			).length;
+			latest = oldest;
+			oldest -= -60 * 60 * 24 * time;
+			const newCount = {
 				messageCount: messageCount,
 				reactionCount: reactionCount,
 			};
-			res.json(result);
-		} else {
-			res.status(400).json("The data not found");
+			result.statistics.push(newCount);
+			res.status(200).json(result);
 		}
-	} else {
-		res.status(400).json("The data not found");
 	}
 });
-
-
 
 export default router;
