62c46d1259784d3db70795c7b7e799e978bf4b84
Diba <72705126+DibaKarimi@users.noreply.github.com>
2021-07-19 20:51:19 +0100

    task1&avrage

--- server/api.js
+++ server/api.js
@@ -4,24 +4,6 @@ import process from "process";
 const router = new Router();
 const axios = require("axios");
 
-// const getChannelList = async () => {
-// 	const slackToken = process.env.SLACK_API_TOKEN;
-// 	const url = `https://ldn7-test-workspace.slack.com/api/conversations.list`;
-// 	const res = await axios.get(url, {
-// 		headers: { Authorization: `Bearer ${slackToken}` },
-// 	});
-// 	return res.data;
-// };
-
-// const getUserList = async () => {
-// 	const slackToken = process.env.SLACK_API_TOKEN;
-// 	const url = "https://ldn7-test-workspace.slack.com/api/users.list";
-// 	const res = await axios.get(url, {
-// 		headers: { Authorization: `Bearer ${slackToken}` },
-// 	});
-// 	return res.data;
-// };
-
 const getUserInfo = async (userId) => {
 	const slackToken = process.env.SLACK_API_TOKEN;
 	const url = `https://ldn7-test-workspace.slack.com/api/users.info?user=${userId}`;
