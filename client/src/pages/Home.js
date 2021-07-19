9de4be671ee58f2a05ece9d11c9e17ad7ad4eb7b
Diba <72705126+DibaKarimi@users.noreply.github.com>
2021-07-19 20:37:22 +0100

    aggregate last week stat and calculate weekly avrage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,47 +1,48 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+// import logo from "./logo.svg";
 import logo from "./cyf_brand.png";
 export function Home() {
-    const [message, setMessage] = useState("Loading...");
-const [statistics,setStatistics] = useState([]);
-    useEffect(() => {
-        fetch("/api/user/U027NPENH9T?time=month")
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(res.statusText);
-                }
-                return res.json();
-            })
-            .then((body) => {
-                setMessage(body.userName);
-                setStatistics(body.statistics);
-                console.log (body);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }, []);
-console.log(statistics[0]);
-    return (
-        <main role="main">
-            <div>
-                <img
-                    className="logo"
-                    data-qa="logo"
-                    src={logo}
-                    alt="Just the React logo"
-                />
-                <h1 className="message" data-qa="message">
-                    {message}
-                </h1>
-                <table>
-                    <th><td>count</td></th>
-                    <tr><td>{message}</td></tr>
-                </table>
-                <Link to="/about/this/site">About</Link>
-            </div>
-        </main>
-    );
+	const [message, setMessage] = useState("Loading...");
+	const [statistics, setStatistics] = useState([]);
+	useEffect(() => {
+		fetch("/api/user/U027NPENH9T?time=week")
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then((body) => {
+				setMessage(body.userName);
+				setStatistics(Object.values(body.statistics[0]));
+				console.log(body);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+	console.log(statistics);
+	return (
+		<main role="main">
+			<div>
+				<img
+					className="logo"
+					data-qa="logo"
+					src={logo}
+					alt="Just the React logo"
+				/>
+				<div className="card">
+					<h1 className="message" data-qa="message">
+						{message}
+					</h1>
+					<h2>Messages: {statistics[0]}</h2>
+					<h2>Reactions: {statistics[1]}</h2>
+				</div>
+				{/* <Link to="/about/this/site">About</Link> */}
+			</div>
+		</main>
+	);
 }
 export default Home;
--- server/api.js
+++ server/api.js
@@ -4,23 +4,23 @@ import process from "process";
 const router = new Router();
 const axios = require("axios");
 
-const getChannelList = async () => {
-	const slackToken = process.env.SLACK_API_TOKEN;
-	const url = `https://ldn7-test-workspace.slack.com/api/conversations.list`;
-	const res = await axios.get(url, {
-		headers: { Authorization: `Bearer ${slackToken}` },
-	});
-	return res.data;
-};
+// const getChannelList = async () => {
+// 	const slackToken = process.env.SLACK_API_TOKEN;
+// 	const url = `https://ldn7-test-workspace.slack.com/api/conversations.list`;
+// 	const res = await axios.get(url, {
+// 		headers: { Authorization: `Bearer ${slackToken}` },
+// 	});
+// 	return res.data;
+// };
 
-const getUserList = async () => {
-	const slackToken = process.env.SLACK_API_TOKEN;
-	const url = "https://ldn7-test-workspace.slack.com/api/users.list";
-	const res = await axios.get(url, {
-		headers: { Authorization: `Bearer ${slackToken}` },
-	});
-	return res.data;
-};
+// const getUserList = async () => {
+// 	const slackToken = process.env.SLACK_API_TOKEN;
+// 	const url = "https://ldn7-test-workspace.slack.com/api/users.list";
+// 	const res = await axios.get(url, {
+// 		headers: { Authorization: `Bearer ${slackToken}` },
+// 	});
+// 	return res.data;
+// };
 
 const getUserInfo = async (userId) => {
 	const slackToken = process.env.SLACK_API_TOKEN;
@@ -29,11 +29,11 @@ const getUserInfo = async (userId) => {
 		headers: { Authorization: `Bearer ${slackToken}` },
 	});
 	return res.data;
-}
+};
 
 const getChannelHistory = async (channel, oldest, latest) => {
 	const slackToken = process.env.SLACK_API_TOKEN;
-	let url = ""
+	let url = "";
 	if(oldest === undefined || latest === undefined) {
 		url = `https://ldn7-test-workspace.slack.com/api/conversations.history?channel=${channel}`;
 	}  else {
@@ -45,30 +45,52 @@ const getChannelHistory = async (channel, oldest, latest) => {
 	return res.data;
 };
 
+const findReaction = (reactions, userId) => {
+	return reactions.filter((reaction) => reaction.users.includes(userId));
+};
 
-router.get("/user/:userId", async (req, res) => {
-	const userId = req.params.userId;
-	let result = {
-		userName: "",
-		statistics: [],
+const getStat = async (userId, oldest,latest) => {
+	latest = latest == undefined? "" : latest;
+	console.log(latest);
+	const data = await getChannelHistory("C027M110K9T", oldest, latest);
+	const messageCount = data.messages.filter(
+		(message) =>
+			(message.user == userId && message.type == "message") ||
+			(message.reply_users && message.reply_users.includes(userId))
+	).length;
+	const reactionCount = data.messages.filter(
+		(message) =>
+			message.reactions && findReaction(message.reactions, userId).length > 0
+	).length;
+	const newCount = {
+		messageCount: messageCount,
+		reactionCount: reactionCount,
 	};
-	const now = new Date();
-	let time = req.query.time == "month" ? now.getDate()-1 : now.getDay()-1;
-	let latest = now / 1000;
-	let oldest = latest - 60 * 60 * 24 * time;
-	time = req.query.time == "month" ? 30 : 7;
-	console.log("duration=", time, " latest=", latest, " oldest=", oldest);
-	const userInfo = await getUserInfo(userId);
-	const userName = userInfo.ok
-		? userInfo.user.real_name
-		: res.status(400).json("The user not found");
-	result.userName = userName;
-	console.log(userName);
-	const findReaction = (reactions) => {
-		return reactions.filter((reaction) => reaction.users.includes(userId));
-	};
-	let data = await getChannelHistory("C028DN05PUG");
-	console.log(data);
+	return newCount;
+};
+
+router.get("/user/:userId", async (req,res)=>{
+const userId = req.params.userId;
+let result = {
+	userName: "",
+	statistics: [],
+};
+const userInfo = await getUserInfo(userId);
+const userName = userInfo.ok
+	? userInfo.user.real_name
+	: res.status(400).json("The user not found");
+result.userName = userName;
+const now = new Date()/1000;
+const oldest =now - 60*60*7*24;
+const newStatistics= await getStat(userId,oldest);
+	result.statistics.push(newStatistics);
+	res.status(200).json(result);
+});
+
+router.get("/avr/:userId", async(req,res)=>{
+const userId=req.params.userId;
+console.log(userId);
+let data = await getChannelHistory("C028DN05PUG");
 	if (data.ok && data.messages) {
 		const timestampOfJoin = data.messages.find(
 			(message) =>
@@ -77,28 +99,16 @@ router.get("/user/:userId", async (req, res) => {
 				message.subtype == "channel_join" &&
 				message.user == userId
 		);
-		console.log(timestampOfJoin.ts);
-		while (latest > timestampOfJoin.ts) {
-			data = await getChannelHistory("C028DN05PUG", oldest, latest);
-			const messageCount = data.messages.filter(
-				(message) =>
-					(message.user == userId && message.type == "message") ||
-					(message.reply_users && message.reply_users.includes(userId))
-			).length;
-			console.log(messageCount);
-			const reactionCount = data.messages.filter(
-				(message) => message.reactions && findReaction(message.reactions)
-			).length;
-			latest = oldest;
-			oldest -= -60 * 60 * 24 * time;
-			const newCount = {
-				messageCount: messageCount,
-				reactionCount: reactionCount,
-			};
-			result.statistics.push(newCount);
-			res.status(200).json(result);
-		}
+		const now=new Date();
+		const joinDate=timestampOfJoin.ts * 1000;
+		const weeks = Math.round((now - joinDate) / 604800000);
+		let stat =await getStat(userId,timestampOfJoin.ts);
+		stat.messageCount=stat.messageCount/weeks;
+		stat.reactionCount=stat.reactionCount/weeks;
+		res.json(stat);
 	}
 });
+
+
 
 export default router;
